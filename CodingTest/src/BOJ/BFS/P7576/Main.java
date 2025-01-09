3a9a52304135b731a32d06cde1b4b5c26b3ccbb8
criling <kim980316@naver.com>
2025-01-09 22:54:21 +0900

    [FIX] - BOJ - P7576

--- CodingTest/src/BOJ/BFS/P7576/Main.java
+++ CodingTest/src/BOJ/BFS/P7576/Main.java
@@ -1,25 +1,36 @@
 package BOJ.BFS.P7576;
 
-import java.util.*;
+
 import java.io.*;
+import java.util.*;
 
+class Tomato {
+    int y;
+    int x;
+    int day;
 
-// 익은 토마토와 인접한(좌,우,위,아래) 토마토는 하루가 지나면 익는다.
-    // 토마토가 없을 수도 있음
-// 며칠이 지나야 모든 토마토가 익는지 최소 일수를 구하자.
+    Tomato(int y, int x, int day){
+        this.y = y;
+        this.x = x;
+        this.day = day;
+    }
+
+}
+
 public class Main {
 
     static int M, N;
     static int[][] box;
-    //좌 우 위 아래
-    static int[] dy = {0,0,-1,1};
-    static int[] dx = {-1,1,0,0};
-    static int numYetTomato;
-    static int numEmpty;
-    static int numDoneTomato;
-    static int answer;
+    static int[][] visited;
+    static StringBuilder sb = new StringBuilder();
+    static int targetTomato;
+    static int answer = Integer.MIN_VALUE;
 
-    static Queue<Tomato> tomatoQueue;
+
+    // 상, 하, 좌, 우
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,91 +38,81 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
 
         box = new int[N][M];
-        numYetTomato = 0;
-        numEmpty = 0;
-        numDoneTomato = 0;
+        visited = new int[N][M];
+        targetTomato = 0;
 
-        tomatoQueue = new LinkedList<>();
+        Queue<Tomato> queue = new LinkedList<>();
 
-
-        int tmp;
-        for(int  n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            for(int m = 0; m < M ; m++){
-                tmp = Integer.parseInt(st.nextToken());
-                box[n][m] = tmp;
-                if(tmp == 1) {
-                    numDoneTomato += 1;
-                    tomatoQueue.add(new Tomato(n, m, 0));
-                } else if (tmp == -1){
-                    numEmpty += 1;
-                } else if (tmp == 0){
-                    numYetTomato += 1;
-                }
-            }
-        }
-
-        answer = 0;
-
-
-        int day = 0;
-        while(!tomatoQueue.isEmpty()){
-            // 1. 큐에서 꺼내옴
-            Tomato tomato = tomatoQueue.poll();
-            day = tomato.day;
-            // 2. 목적지 인가
-            // 3. 연결된 곳 순회
-            for(int i = 0; i < 4; i++){
-                // 4. 갈 수 있는가
-                if(tomato.y + dy[i] < N
-                        && tomato.y + dy[i] >= 0
-                        && tomato.x + dx[i] < M
-                        && tomato.x + dx[i] >= 0
-                        && box[tomato.y + dy[i]][tomato.x + dx[i]] == 0) {
-                    // 5. 체크인
-                    // 6. 큐에 넣는다.
-                    box[tomato.y + dy[i]][tomato.x + dx[i]] = 1;
-                    tomatoQueue.add(new Tomato(tomato.y + dy[i], tomato.x + dx[i], tomato.day+1));
-                }
-            }
-            answer = day;
-        }
-        // 7. 체크아웃
-
-
-        if (checkTomatoBox()) {
-            System.out.println(answer);
-        } else {
-            System.out.println(-1);
-        }
-
-    }
-
-    static boolean checkTomatoBox() {
         for(int n = 0; n < N; n++){
-            for(int m = 0; m < M; m++) {
-                if (box[n][m] == 0) {
-                    return false;
+            st = new StringTokenizer(br.readLine());
+            for(int m = 0; m < M; m++){
+                int tmp = Integer.parseInt(st.nextToken());
+                if(tmp == 1) {
+                    queue.add(new Tomato(n, m, 0));
+                    visited[n][m] = 1;
+                } else if(tmp == 0){
+                    targetTomato += 1;
                 }
+                box[n][m] = tmp;
             }
         }
-        return true;
+
+
+        if(targetTomato == 0){
+            sb.append(0);
+        } else {
+            bfs(queue);
+
+            if(targetTomato == 0){
+                sb.append(answer);
+            } else if(targetTomato != 0){
+                sb.append(-1);
+            }
+        }
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 
-    static class Tomato {
-        int y;
-        int x;
-        int day;
-        Tomato(int y, int x, int day){
-            this.y = y;
-            this.x = x;
-            this.day = day;
+    static void bfs(Queue<Tomato> queue){
+
+        while(!queue.isEmpty()) {
+            // 1. 큐에서 꺼내온다.
+            Tomato tomato = queue.poll();
+
+            // 2. 목적지인가
+            // 3. 연결된 곳 순회
+            int my = tomato.y;
+            int mx = tomato.x;
+            int day = tomato.day;
+
+            for(int i = 0; i < 4; i++) {
+                int ty = my + dy[i];
+                int tx = mx + dx[i];
+
+                // 4. 갈 수 있는가
+                if(ty < 0 || ty >= N || tx < 0 || tx >= M) {
+                    continue;
+                }
+
+                if(box[ty][tx] == 0 && visited[ty][tx] == 0) {
+                    // 5. 체크인
+                    visited[ty][tx] = 1;
+
+                    // 6. 큐에 넣는다.
+                    queue.add(new Tomato(ty, tx, day + 1));
+                    answer = day + 1;
+                    targetTomato -= 1;
+                }
+            }
+
+            // 7. 체크아웃
         }
     }
 }
-
